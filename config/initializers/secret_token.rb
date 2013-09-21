edebbc146ed578d39001452e0e6e2b7cdbb9e4a4
Garrett Weber <gweber0285@gmail.com>
2013-09-20 22:14:55 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Covridge::Application.config.secret_token = '99da3b2e1035997d988d50e399f66fafdc2c29d5eadeaf8e634b57d0e501456bdeee1d346070ebc76c3a1c5af2fd84bc71f05468cc25f707f2b5cd93ed0182a1'
