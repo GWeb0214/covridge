3c3a6aab077f9262bb0da7cf250a85b88850f8c6
Garrett Weber <gweber0285@gmail.com>
2013-09-20 23:14:16 -0400

    Rough Site

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def howitworks
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Covridge::Application.routes.draw do
+  devise_for :users
+
+  root :to => 'pages#home'
+  get 'about' => 'pages#about'
+  get 'howitworks' => 'pages#howitworks'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
